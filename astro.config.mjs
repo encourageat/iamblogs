6b1f4d87f4157fd0ba820f62c0409a73d5ee6e9c
George Thomas <george.thomas@skycloak.io>
2026-07-02 18:23:23 +0530

    changed to custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,8 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://encourageat.github.io',
-    base: '/iamblogs',
+	site: 'https://blog.encourageat.com',
+    base: '/',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
